ef6cbe1d648c81d472c44840d3b50844928b9d23
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-11-15 13:48:38 +0000

    [Add] newページ作成中

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,24 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    @post.save
+    redirect_to post_path
   end
 
   def index
   end
 
   def show
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_11_15_112153) do
+ActiveRecord::Schema.define(version: 2024_11_15_132755) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2024_11_15_112153) do
     t.boolean "is_active", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
