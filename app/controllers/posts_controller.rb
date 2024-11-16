5d5b59a97789cf14932656e116e7771545fffb9f
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-11-16 12:42:59 +0000

    [Add] indexページ作成完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,10 +7,11 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     @post.save
-    redirect_to post_path
+    redirect_to posts_path
   end
 
   def index
+    @posts = Post.all
   end
 
   def show
