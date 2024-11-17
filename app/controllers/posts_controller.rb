5b307525a0a3442174d91a47b001987687eafe99
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-11-17 02:35:27 +0000

    [Add] ユーザー制限中

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,9 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+     if @post.user != current_user
+    redirect_to posts_path, alert: '他のユーザーの投稿は編集できません。'
+    end
   end
 
   def update
