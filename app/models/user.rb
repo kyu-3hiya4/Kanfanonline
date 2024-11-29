cb4ca5b310ac56fe753f5653e2afa2d2ac9a0962
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-11-29 11:49:04 +0000

    [Update]　userのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
- 
   has_many :posts, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
