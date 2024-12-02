65fd9036c70b7cbf1eaee9f70c8dca5b7ec6cc50
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-02 12:52:49 +0000

    [Add]　userとpostモデルに検索記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :email, presence: true
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts
   resources :users, only: [:show, :edit, :update, :destroy]
-  get 'search', to: 'searches#search'
+  get '/search', to: 'searches#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
