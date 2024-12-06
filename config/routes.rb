220e84c18afb78205d90d74cf6c3af3e606edba4
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-06 12:16:52 +0000

    [Add] ユーザー一覧作成中

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+
+  namespace :admin do
+    get 'dashboards', to: 'dashboards#index'
+  end
   
   root to: "homes#top"
   devise_for :users
