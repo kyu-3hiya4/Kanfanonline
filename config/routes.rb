b20eb5ba4a2df96b15fe66a4f1a4b31e5aee7880
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-08 11:55:24 +0000

    [Fix] ファイル整理

--- config/routes.rb
+++ config/routes.rb
@@ -13,18 +13,17 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     devise_for :users
-
     resources :posts do
       resources :comments, only: [:create, :destroy]
     end
-    
     resources :users, only: [:show, :edit, :update, :destroy]
+    get '/search', to: 'searches#search'
   end
 
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
 
-  get '/search', to: 'searches#search'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
