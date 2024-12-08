09960417543efbb81f5ae6d7f6874a9107680ac3
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-08 00:55:41 +0000

    [Add] applicationcontroller編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,21 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top]
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def after_sign_in_path_for(resource)
-    user_path(current_user.id)
+  before_action :configure_authentication
+ 
+  private
+ 
+  def configure_authentication
+    if admin_controller?
+      authenticate_admin!
+    else
+      authenticate_user! unless action_is_public?
+    end
   end
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+ 
+  def admin_controller?
+    self.class.module_parent_name == 'Admin'
+  end
+ 
+  def action_is_public?
+    controller_name == 'homes' && action_name == 'top'
   end
 end
