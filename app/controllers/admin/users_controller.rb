2963ef90dd1cfbff2606130221b2676a10b7377f
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-09 11:32:56 +0000

    [Add] ヘッダー作成とユーザー一覧ページ作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,10 @@
 class Admin::UsersController < ApplicationController
+  layout 'admin'
 
+  def index
+   @users = User.all
+  end
+  
   def show
     @user = User.find(params[:id])
     # @posts = @user.posts
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:show, :destroy] 
+    resources :users, only: [:index, :show, :destroy] 
     resources :comments, only: [:index, :destroy]
   end
   
