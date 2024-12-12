c4905902a4fdadd163d6c547e7c3fb4f89789638
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-12 12:40:21 +0000

    [Fix] コメントバリデーションととメッセージ

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,12 +2,15 @@ class Public::CommentsController < ApplicationController
 
   def create
     post = Post.find(params[:post_id])
-    @body = current_user.comments.new(comment_params)
-    @body.post_id = post.id
-    if @body.save
+    body = current_user.comments.new(comment_params)
+    body.post_id = post.id
+    if body.save
       redirect_to post_path(post)
     else
-      redirect_to post_path(post)  
+      @error_comment = body
+      @post = Post.find(params[:post_id])
+      @comment = Comment.new
+      render 'public/posts/show' 
     end
   end
 
