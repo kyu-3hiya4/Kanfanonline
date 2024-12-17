5460e9f0325b61a71471f5a5bf2464a218e4db0c
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-17 13:14:20 +0000

    [Add] group/create作成

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -3,9 +3,24 @@ class Public::GroupsController < ApplicationController
     @group = Group.new
   end
 
+  def create
+    @group = Group.new(group_params)
+    @group.owner_id = current_user.id
+    if @group.save
+      redirect_to groups_path
+    else
+      render 'new'
+     end
+    end
+
   def index
   end
 
   def show
   end
+
+  private
+   def group_params
+    params.require(:group).permit(:name, :theme)
+   end
 end
