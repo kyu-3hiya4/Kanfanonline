358da78233d4016d093883c5fde21f4dc01cf38e
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-19 07:51:14 +0000

    [Add] userとpostとgroupに権限

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -1,4 +1,7 @@
 class Public::GroupsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_user, only: [:edit, :update]
+
   def new
     @group = Group.new
   end
