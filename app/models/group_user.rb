3ea244b59bab8deae901e8e32c1127ed722eb454
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2024-12-26 12:10:50 +0000

    [Add] group_usersテーブルにstatusカラム追加

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -1,4 +1,6 @@
 class GroupUser < ApplicationRecord
   belongs_to :group
   belongs_to :user
+
+  enum status: { pending:0, approval: 1, cancel: 2 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_12_16_111602) do
+ActiveRecord::Schema.define(version: 2024_12_26_115247) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2024_12_16_111602) do
     t.integer "group_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0, null: false
   end
 
   create_table "groups", force: :cascade do |t|
