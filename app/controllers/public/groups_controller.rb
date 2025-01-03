54519f8b5af9db6d10dfdeb15bf07c978a4f25ad
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2025-01-03 07:31:39 +0000

    [Fix] postに更新リンク追加

--- app/controllers/public/groups_controller.rb
+++ app/controllers/public/groups_controller.rb
@@ -10,7 +10,7 @@ class Public::GroupsController < ApplicationController
     @group = Group.new(group_params)
     @group.owner_id = current_user.id
     if @group.save
-      redirect_to groups_path
+      redirect_to group_path(@group.id)
     else
       render 'new'
      end
@@ -32,11 +32,12 @@ class Public::GroupsController < ApplicationController
     @group = Group.find_by(id: params[:id])
     if @group.update(group_params)
        redirect_to group_path(@group.id) 
+
     else
       render :edit
     end
   end
-  
+
   private
    def group_params
     params.require(:group).permit(:name, :theme)
