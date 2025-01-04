2d123a479491908c5f75bb8bebff9bf39974ef18
EC2 Default User <ec2-user@ip-172-31-33-153.eu-north-1.compute.internal>
2025-01-04 05:43:53 +0000

    [Fix] グループ申請修正

--- app/models/group_user.rb
+++ app/models/group_user.rb
@@ -2,5 +2,5 @@ class GroupUser < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  enum status: { pending:0, approval: 1, cancel: 2 }
+  enum status: { pending: 0, approval: 1, cancel: 2 }
 end
